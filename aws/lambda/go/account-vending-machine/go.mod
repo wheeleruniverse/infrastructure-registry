0fccc1330ecc8e2be467361e46387d6f8e34a1ca
Justin Wheeler <wheelers_webistes@yahoo.com>
2022-10-25 08:09:06 -0400

    account-vending-machine Go Lambda AssumeRole func

--- aws/lambda/go/account-vending-machine/go.mod
+++ aws/lambda/go/account-vending-machine/go.mod
@@ -4,13 +4,13 @@ go 1.19
 
 require (
 	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go-v2 v1.17.0
 	github.com/aws/aws-sdk-go-v2/config v1.17.0
-	github.com/aws/aws-sdk-go-v2/service/cloudformation v1.22.4
 	github.com/aws/aws-sdk-go-v2/service/organizations v1.16.14
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.13
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.17.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.13 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.24 // indirect
@@ -18,7 +18,5 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.19 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.12 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.16 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.16.13 // indirect
 	github.com/aws/smithy-go v1.13.3 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- aws/lambda/go/account-vending-machine/main.go
+++ aws/lambda/go/account-vending-machine/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/organizations"
 	"github.com/aws/aws-sdk-go-v2/service/organizations/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"log"
 )
 
@@ -20,30 +21,55 @@ func HandleRequest() (string, error) {
 		log.Fatalf("LoadDefaultConfig failed because %v", cfgErr)
 	}
 
+	account := ""
+	domain := ""
+	environment := ""
+	role := "OrganizationAccountAccessRole"
+
 	organizationsClient := organizations.NewFromConfig(cfg)
-	createAccount(organizationsClient, "", "", "")
+	createAccountOutput := createAccount(organizationsClient, account, domain, environment, role)
+	accountId := aws.ToString(createAccountOutput.CreateAccountStatus.AccountId)
 
 	// TODO move account to OU
 
-	// TODO assume role for new account
+	stsClient := sts.NewFromConfig(cfg)
+	assumeRole(stsClient, accountId, role)
 
 	// TODO with assumed role delete default vpc
 
 	return "Success", nil
 }
 
+func assumeRole(
+	client *sts.Client,
+	accountId string,
+	role string,
+) *sts.AssumeRoleOutput {
+	assumeRoleInput := sts.AssumeRoleInput{
+		RoleArn:         aws.String("arn:aws:iam::" + accountId + ":role/" + role),
+		RoleSessionName: aws.String("NewAccountRole"),
+		DurationSeconds: aws.Int32(900),
+	}
+	assumeRoleOutput, assumeRoleErr := client.AssumeRole(context.TODO(), &assumeRoleInput)
+	if assumeRoleErr != nil {
+		log.Fatalf("sts.AssumeRole failed because %v", assumeRoleErr)
+	}
+	return assumeRoleOutput
+}
+
 func createAccount(
-	organizationsClient *organizations.Client,
+	client *organizations.Client,
 	account string,
 	domain string,
 	environment string,
+	role string,
 ) *organizations.CreateAccountOutput {
 	email := aws.String(account + "@wheelerswebservices.com")
 	createAccountInput := organizations.CreateAccountInput{
 		AccountName:            &account,
 		Email:                  email,
 		IamUserAccessToBilling: types.IAMUserAccessToBillingDeny,
-		RoleName:               aws.String("OrganizationAccountAccessRole"),
+		RoleName:               &role,
 		Tags: []types.Tag{
 			{
 				Key:   aws.String("Account"),
@@ -63,7 +89,7 @@ func createAccount(
 			},
 		},
 	}
-	createAccountOutput, createAccountErr := organizationsClient.CreateAccount(context.TODO(), &createAccountInput)
+	createAccountOutput, createAccountErr := client.CreateAccount(context.TODO(), &createAccountInput)
 	if createAccountErr != nil {
 		log.Fatalf("organizations.CreateAccount failed because %v", createAccountErr)
 	}
